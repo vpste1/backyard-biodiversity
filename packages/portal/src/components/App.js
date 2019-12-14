fde5f3a8e659f3c0f13c19ab3b368e8a944e6b62
Vaughan Stedman <vaughan.stedman@gmail.com>
2019-12-14 11:12:04 +1100

    chore: set up styling structure, include logos, cleanup

--- packages/portal/src/components/App.js
+++ packages/portal/src/components/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-import logo from "../logo.svg";
-import "./App.scss";
 
 import Form from "./form/Form";
+
+import logo from "../assets/images/logo_128.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} />
       <Form />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- packages/portal/src/index.js
+++ packages/portal/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import "./assets/styles/_index.scss";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
