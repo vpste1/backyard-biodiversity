aff71c623d4d25b8669b8198684b97df73a26fd9
Vaughan Stedman <vaughan.stedman@gmail.com>
2019-12-20 08:55:57 +1100

    wip: update styling of login form

--- packages/portal/src/components/App.js
+++ packages/portal/src/components/App.js
@@ -2,12 +2,9 @@ import React from "react";
 
 import LoginForm from "./LoginForm";
 
-import logo from "../assets/images/logo_128.png";
-
 function App() {
   return (
     <div className="App">
-      <img src={logo} />
       <LoginForm />
     </div>
   );
--- packages/portal/src/components/LoginForm.js
+++ packages/portal/src/components/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import FIELDS from "common/resources/fields";
 import "./LoginForm.scss";
 
+import logo from "../assets/images/logo_128.png";
+
 function LoginForm() {
   const [formData, setFormData] = useState({
     username: "",
@@ -10,28 +12,33 @@ function LoginForm() {
 
   return (
     <div className="login__container">
-      <h1>Login</h1>
-      <form className="login__form">
-        <input
-          type="text"
-          className="login__input"
-          placeholder="Email"
-          value={formData.username}
-          onChange={event =>
-            setFormData({ ...formData, username: event.target.value })
-          }
-        />
-        <input
-          type="password"
-          className="login__input"
-          placeholder="Password"
-          value={formData.password}
-          onChange={event =>
-            setFormData({ ...formData, password: event.target.value })
-          }
-        />
-        <input className="login__btn" type="submit" value="Login" />
-      </form>
+      <div className="login__logo">
+        <img src={logo} />
+      </div>
+      <div className="login__body">
+        <h1>Login</h1>
+        <form className="login__form">
+          <input
+            type="text"
+            className="login__input"
+            placeholder="Email"
+            value={formData.username}
+            onChange={event =>
+              setFormData({ ...formData, username: event.target.value })
+            }
+          />
+          <input
+            type="password"
+            className="login__input"
+            placeholder="Password"
+            value={formData.password}
+            onChange={event =>
+              setFormData({ ...formData, password: event.target.value })
+            }
+          />
+          <input className="btn login__btn" type="submit" value="Login" />
+        </form>
+      </div>
     </div>
   );
 }
