4ea2b75b24869fcfb2892a1eb28431447c7dd209
Vaughan <vaughan.stedman@gmail.com>
2020-02-15 11:16:44 +1100

    chore: swap out cognito for auth0

--- packages/portal/src/components/App.js
+++ packages/portal/src/components/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Route } from "wouter";
-
-import LoginForm from "./auth/LoginForm";
-import VerifyForm from "./auth/VerifyForm";
-import SignUpForm from "./auth/SignUpForm";
-import { useAuth } from "../context/authContext";
+import NavBar from "./NavBar";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./Profile";
+import PrivateRoute from "./PrivateRoute";
+import history from "../utils/history";
 
 function App() {
-  const { login, verify, signUp } = useAuth();
   return (
     <div className="App">
-      <Route path="/login">
-        <LoginForm onSubmit={login} />
-      </Route>
-      <Route path="/sign-up">
-        <SignUpForm onSubmit={signUp} />
-      </Route>
-      <Route path="/verify">
-        <VerifyForm onSubmit={verify} />
-      </Route>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- packages/portal/src/context/index.js
+++ packages/portal/src/context/index.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { AuthProvider } from "./authContext";
+import config from "../config/auth.json";
+import { Auth0Provider } from "./authContext";
+import history from "../utils/history";
+
+// Routes the user after login
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 function AppProviders({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 }
 
 export default AppProviders;
